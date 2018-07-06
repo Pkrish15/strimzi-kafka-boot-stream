1d541d20c6e7480b47ea127086305b68d3effd4b
root <root@localhost.localdomain>
2018-07-06 12:53:52 +0530

    Initial Commit for Kafka Producer Example with Spring-Boot

---
+++ src/main/java/com/redhat/gpte/SpringBootKafkaProducerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.gpte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProducerExampleApplication.class, args);
+	}
+}
